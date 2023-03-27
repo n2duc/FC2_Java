a94f8d348c3f775d127c3a2a619db8cda772c26f
Nguyen Duc <97241208+n2duc@users.noreply.github.com>
2023-03-27 19:11:47 +0700

    linked

--- FC2_Java/src/main/java/n2d/Linked.java
+++ FC2_Java/src/main/java/n2d/Linked.java
@@ -37,7 +37,7 @@ public class Linked {
             System.out.print(p);
             p = p.next;
         }
-        System.out.print("null");
+        System.out.print("null\n");
     }
     void inputList() {
         Scanner sc = new Scanner(System.in);
@@ -102,18 +102,68 @@ public class Linked {
             p.next = t;
         }
     }
-    void deleteNode(int k) {
-        
+    void insert(int x, int k) {
+        if (k < 1) System.out.println("Vi tri chen khong hop le!");
+        else if (k == 1) head = new Node(x, head);
+        else {
+            int vt = 1;
+            Node p = head;
+            while(p!=null && vt < k - 1) {
+                vt++; p = p.next;
+            }
+            if (p == null) System.out.println("Vi tri chen khong hop le!");
+            else {
+                Node t = new Node(x, p.next); p.next = t;
+            }
+        }
+    }
+    void deleteFirst() {
+        if (head == null) System.out.println("Khong xoa duoc 1");
+        else head = head.next;
+    }
+    void deleteEnd() {
+        if (head == null) System.out.println("Khong xoa duoc");
+        else if (head.next == null) head = null;
+        else {
+            Node p = head;
+            while (p.next.next != null) {
+                p = p.next;
+            }
+            p.next = null;
+        }
+    }
+    void deleteK (int k) {
+        if (head == null) System.out.println("Khong xoa duoc");
+        else if (k == 1) {
+            head = head.next;
+        } else {
+            int vt = 1;
+            Node p = head;
+            while (p != null && vt < k - 1) {
+                vt++;
+                p = p.next;
+            }
+            if (p == null || p.next == null) {
+                System.out.println("Khong xoa duoc");
+            } else {
+                p.next = p.next.next;
+            }
+        }
     }
     public static void main(String[] args) {
         Linked l = new Linked();
-        l.add(3); l.add(5); l.add(6); l.add(9); l.add(1);
-//        l.inputList();
+//        l.add(3); l.add(5); l.add(6); l.add(9); l.add(1);
+        l.inputList();
         System.out.println("Number of Node: " + l.countNode());
         System.out.println("Total number of Node: " + l.totalNode());
         System.out.println("Odd number of Node: " + l.oddNode());
         System.out.println("Max number of Node: " + l.maxNode());
         l.appendNode(10);
+        l.insert(8, 3);
+        l.in();
+        l.deleteFirst();
+        l.deleteEnd();
+        l.deleteK(2);
         l.in();
     }
 }
